da3efb9959033b5543241f12d07cc378bc22c42a
Eric-Domeier <mortyboo2@pm.me>
2025-09-29 21:33:25 -0400

    Fix lint, remove unwanted files

--- cmd/generate/oscal/component/component.go
+++ cmd/generate/oscal/component/component.go
@@ -10,6 +10,7 @@ import (
 func NewCmd() *cobra.Command {
 	var inputFile string
 	var outputFile string
+	var cciFile string
 
 	cmd := &cobra.Command{
 		Use:   "component",
@@ -17,12 +18,13 @@ func NewCmd() *cobra.Command {
 		Long: `Generate an OSCAL component definition from a STIG checklist.
 This command takes a STIG checklist as input and converts it to an OSCAL component definition.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return component.GenerateComponent(inputFile, outputFile)
+			return component.GenerateComponent(inputFile, outputFile, cciFile)
 		},
 	}
 
 	cmd.Flags().StringVarP(&inputFile, "input", "i", "", "Path to the STIG checklist file")
 	cmd.Flags().StringVarP(&outputFile, "output", "o", "component.json", "Path to the output OSCAL component file")
+	cmd.Flags().StringVarP(&cciFile, "cci-file", "cci", "", "Path to the CCI file (optional, will use embedded if not provided)")
 	_ = cmd.MarkFlagRequired("input")
 
 	return cmd
